8ca33954b4f6ecc45fe7706d705518ecb26a9ba7
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2025-05-11 14:54:40 +0530

    implemented controller

--- src/main/java/com/chat/chatApplication/config/WebSocketEventListener.java
+++ src/main/java/com/chat/chatApplication/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.chat.chatApplication.config;
 
+import com.chat.chatApplication.chat.ChatMessage;
+import com.chat.chatApplication.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,10 +15,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messagingTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ){
-        // TODO -- to be implemented
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString();
+        if(username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVER)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
